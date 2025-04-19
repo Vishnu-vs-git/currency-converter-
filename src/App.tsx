928648f5cd5798240c38fdc65c491df298b3f617
vishnu <getvishnuvs@gmail.com>
2025-04-19 11:27:49 +0530

    implements fetch  currencies from Api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import CurrencyConverter from './components/Currency-Converter'
-
+import { ToastContainer } from 'react-toastify';
 const App = () => {
   return (
     <div className='min-h-screen bg-gray-100 flex flex-col items-center justify-center '>
--- src/components/Currency-Converter.tsx
+++ src/components/Currency-Converter.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { toast } from 'react-toastify';
 
 // https://api.frankfurter.dev/v1/currencies //==============> api for currencies
 //  https://api.frankfurter.dev/v1/latest?from=USD&to=INR
 
 const CurrencyConverter = () => {
+ const[currencies,setCurrencies] = useState([])
+ const[amount,setAmount] = useState(1);
+
+
+
+
+  const fetchCurrency=async()=>{
+    
+    try{
+      const res= await fetch("https://api.frankfurter.dev/v1/currencies")
+      const  data= await res.json()
+      toast.success("Converted!", {
+        className:
+          "bg-indigo-600 text-white text-md font-medium rounded-lg px-4 py-3 shadow-md",
+        progressClassName: "bg-green-400",
+      });
+      
+      setCurrencies(data)
+    }catch(error:any){
+      toast.error(`Error: ${error.message || "Something went wrong!"}`, {
+        className: "bg-red-600 text-white font-semibold rounded-lg shadow-md",
+      });
+        console.error("Error Fetching ")
+    }
+  } 
+
+  useEffect(()=>{
+    fetchCurrency()
+  },[])
+  console.log(currencies)
+
+
+
+
+
+
   return (
     <div className="max-w-xl mx-auto my-10 p-5 bg-white rounded-lg shadow-md ">
       <h2 className="mb-5 text-2xl  font-semibold text-gray-700">
@@ -15,9 +52,11 @@ const CurrencyConverter = () => {
           className="block text-sm font-medium text-gray-700"
           htmlFor="amount"
         >
-          Amount
+          Amount:
         </label>
         <input
+        value={amount}
+          onChange={(e)=>setAmount(Number(e.target.value))}
           className="w-full p-2 border border-gray-300 rounded-md shadow-sm ocus:outline-none focus:ring-indigo-500 "
           type="number"
         />
